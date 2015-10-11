9440842288b887f5df1cce93cddff8f652995338
xcsoft <xcsoft@leaguesharp.xcsoft.pusku.com>
2015-10-11 09:47:38 +0900

    ?

--- SharpShooter/Plugins/Kalista.cs
+++ SharpShooter/Plugins/Kalista.cs
@@ -62,7 +62,7 @@ namespace SharpShooter.Plugins
             Spellbook.OnCastSpell += Spellbook_OnCastSpell;
             Obj_AI_Base.OnProcessSpellCast += Obj_AI_Base_OnProcessSpellCast;
 
-            BaronLocation = new Vector3(4910f, 10385f, -71f);
+            BaronLocation = new Vector3(4979f, 10475f, -71f);
             DragonLocation = new Vector3(9796f, 4432f, -71f);
 
             Console.WriteLine("Sharpshooter: Kalista Loaded.");
@@ -170,7 +170,7 @@ namespace SharpShooter.Plugins
                             if (MenuProvider.Champion.Laneclear.UseE)
                                 if (E.isReadyPerfectly())
                                     if (ObjectManager.Player.isManaPercentOkay(MenuProvider.Champion.Laneclear.IfMana))
-                                        if (MinionManager.GetMinions(E.Range).Count(x => x.isKillableAndValidTarget(E.GetDamage(x))) >= MenuProvider.Champion.Laneclear.getSliderValue("Cast E if Killable Minion Number >=").Value)
+                                        if (MinionManager.GetMinions(E.Range).Count(x => HealthPrediction.GetHealthPrediction(x, 250) < 0 && x.isKillableAndValidTarget(E.GetDamage(x))) >= MenuProvider.Champion.Laneclear.getSliderValue("Cast E if Killable Minion Number >=").Value)
                                             E.Cast();
 
                             //Jugnle
@@ -247,7 +247,7 @@ namespace SharpShooter.Plugins
                 if (MenuProvider.Champion.Misc.getBoolValue("Use Lasthit Assist (With E)"))
                     if (E.isReadyPerfectly())
                         if (Minion.isKillableAndValidTarget(E.GetDamage(Minion)))
-                            if (HealthPrediction.GetHealthPrediction(Minion, 100) < 0)
+                            if (HealthPrediction.GetHealthPrediction(Minion, 250) < 0)
                                 if (!HeroManager.Enemies.Any(x => Orbwalking.InAutoAttackRange(x)))
                                     E.Cast();
             }
