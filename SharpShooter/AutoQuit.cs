34f68f90cab161ea3633db0df0bd808472a77930
xcsoft <xcsoft@leaguesharp.xcsoft.pusku.com>
2015-10-19 21:42:02 +0900

    ?

--- SharpShooter/AutoQuit.cs
+++ SharpShooter/AutoQuit.cs
@@ -1,21 +1,37 @@
-﻿using LeagueSharp;
+﻿using System;
+using LeagueSharp;
 using LeagueSharp.Common;
 
 namespace SharpShooter
 {
     class AutoQuit
     {
+        private static int ENDTIME = 0;
+        private static bool DONE = false;
+
         internal static void Load()
         {
             MenuProvider.MenuInstance.AddItem(new MenuItem("QuitTheGameAfterGameOver", "Auto Quit the game after game over")).SetValue(true);
 
+            Game.OnUpdate += Game_OnUpdate;
             Game.OnEnd += Game_OnEnd;
+        }
+
+        private static void Game_OnUpdate(System.EventArgs args)
+        {
+            if (ENDTIME == 0 || DONE == true)
+                return;
+
+            if (ENDTIME + 3000 <= Environment.TickCount)
+            {
+                DONE = true;
+                Game.Quit();
+            }
         }
 
         private static void Game_OnEnd(GameEndEventArgs args)
         {
-            if (MenuProvider.MenuInstance.Item("QuitTheGameAfterGameOver").GetValue<bool>())
-                Game.Quit();
+            ENDTIME = Environment.TickCount;
         }
     }
 }
